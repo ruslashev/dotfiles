3b52029e532ad6475c587c3045927d71aff29a2a
Ruslan Akbashev <ruslashev@gmail.com>
2019-09-26 17:53:01 +0300

    add speedcrunch to packages.sh

--- install-scripts/packages.sh
+++ install-scripts/packages.sh
@@ -2,5 +2,5 @@
 
 sudo apt install -y gcc git curl python3-pip vim-gtk3 exuberant-ctags cmake \
     libclang-dev pkg-config libfreetype6-dev libfontconfig1-dev \
-    libxcb-xfixes0-dev python3 tmux
+    libxcb-xfixes0-dev python3 tmux speedcrunch
 

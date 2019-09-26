b6037e710414e4e808b5c1fc35ecea0f2d572880
Ruslan Akbashev <ruslashev@gmail.com>
2019-09-26 17:42:52 +0300

    vim.sh: don't install packages since packages.sh does it

--- install-scripts/vim.sh
+++ install-scripts/vim.sh
@@ -4,8 +4,6 @@ mkdir ~/.vim/
 mkdir ~/.vim/colors
 mkdir ~/.vim/undo
 
-sudo apt -y install curl python3-pip ctags cmake
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 

9acdf96ea0c9439c199fc4c32453bbfd3ddfd9fc
Ruslan Akbashev <ruslashev@gmail.com>
2019-09-26 17:41:10 +0300

    add tmux to packages.sh

--- install-scripts/packages.sh
+++ install-scripts/packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt install -y gcc git curl python3-pip vim-gtk3 exuberant-ctags cmake \
-    libclang-dev pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+    libclang-dev pkg-config libfreetype6-dev libfontconfig1-dev \
+    libxcb-xfixes0-dev python3 tmux
 

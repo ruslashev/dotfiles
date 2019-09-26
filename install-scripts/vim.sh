78dbfca024cbada4a2bb2c54853866dbcf5aa716
Ruslan Akbashev <ruslashev@gmail.com>
2019-09-26 17:22:59 +0300

    install-scripts/vim.sh: make directory-agnostic

--- install-scripts/vim.sh
+++ install-scripts/vim.sh
@@ -9,7 +9,7 @@ sudo apt -y install curl python3-pip ctags cmake
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cp vim/colors/solarized.vim ~/.vim/colors
+cp ~/dotfiles/vim/colors/solarized.vim ~/.vim/colors
 
 vim -c PlugInstall ~/.vimrc
 

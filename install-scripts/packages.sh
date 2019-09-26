0aba79fef37fb48fedc6623d1369fceb5bdef492
Ruslan Akbashev <ruslashev@gmail.com>
2019-09-26 17:37:28 +0300

    add packages.sh that installs ...packages

---
+++ install-scripts/packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y gcc git curl python3-pip vim-gtk3 exuberant-ctags cmake \
+    libclang-dev pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+

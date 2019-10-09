3ad377ed8c316f7fb41714c665a4df0116bc18d3
Ruslan Akbashev <ruslashev@gmail.com>
2019-10-09 16:42:08 +0300

    packages.sh: add valgrind

--- install-scripts/packages.sh
+++ install-scripts/packages.sh
@@ -2,5 +2,6 @@
 
 sudo apt install -y gcc git curl python3-pip vim-gtk3 exuberant-ctags cmake \
     libclang-dev pkg-config libfreetype6-dev libfontconfig1-dev \
-    libxcb-xfixes0-dev python3 tmux speedcrunch xclip tree net-tools gdb
+    libxcb-xfixes0-dev python3 tmux speedcrunch xclip tree net-tools gdb \
+    valgrind
 

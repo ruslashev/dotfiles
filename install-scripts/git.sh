acb16a893590197ed7e4f399f4ba1d25483047b8
Ruslan Akbashev <ruslashev@gmail.com>
2019-10-31 20:27:31 +0300

    git.sh: add diff-highlight and better diff colors

--- install-scripts/git.sh
+++ install-scripts/git.sh
@@ -2,3 +2,23 @@
 
 git config --global core.excludesfile ~/dotfiles/gitignore
 
+binary=$(find -L /usr -name diff-highlight -type f 2> /dev/null | head -n 1)
+
+git config --global core.pager $binary
+
+cat <<HERE >> ~/.gitconfig
+[color "diff"]
+	commit = blue
+	meta = yellow
+	frag = cyan
+	old = red
+	new = green
+	whitespace = red reverse
+
+[color "diff-highlight"]
+	oldNormal = red
+	oldHighlight = red 52
+	newNormal = green
+	newHighlight = green 22
+
+HERE

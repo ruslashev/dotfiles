335d322d3026b05fb858e60800d07a8ceb67c00e
Ruslan Akbashev <ruslashev@gmail.com>
2021-06-04 17:25:04 +0300

    scripts: support new archived distribution of rust-analyzer

--- scripts/rust-update.sh
+++ scripts/rust-update.sh
@@ -5,9 +5,13 @@ rustup update stable
 
 rustup component add clippy rust-src
 
+baseurl='https://github.com/rust-analyzer/rust-analyzer/releases/latest/download'
+binary='rust-analyzer-x86_64-unknown-linux-gnu.gz'
+tempdir=$(mktemp -d)
+
 mkdir -p ~/bin
 cd ~/bin || exit
-curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux \
-    -o ~/bin/rust-analyzer
+curl -L "$baseurl/$binary" -o "$tempdir/$binary"
+gzip -d "$tempdir/$binary" -c > ~/bin/rust-analyzer
 chmod +x ~/bin/rust-analyzer
 

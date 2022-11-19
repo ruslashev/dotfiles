368d9b27e7d751614d6f369dfc2984a4dc686eba
Ruslan Akbashev <ruslashev@gmail.com>
2022-11-19 07:30:37 +0400

    scripts/packages: add some packages from new system install

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,5 +3,6 @@
 sudo apt install -y gcc git curl python3-pip vim-gtk3 exuberant-ctags cmake \
     libclang-dev pkg-config libfreetype6-dev libfontconfig1-dev \
     libxcb-xfixes0-dev python3 tmux speedcrunch xclip tree net-tools gdb \
-    valgrind
+    valgrind clang fd-find jq gpg bless meld ripgrep xorriso mtools grub-pc-bin \
+    gedit telegram-desktop gdmap sl htop strace ltrace gpg
 

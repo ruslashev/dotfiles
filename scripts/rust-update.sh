a5daf2bfcf801308ba6e2586ecc419849e8c7a37
Ruslan Akbashev <ruslashev@gmail.com>
2023-02-14 06:33:11 +0400

    scripts: don't install rust-analyzer
    This is a job of coc-rust-analyzer now

--- scripts/rust-update.sh
+++ scripts/rust-update.sh
@@ -5,13 +5,3 @@ rustup update stable
 
 rustup component add clippy rust-src
 
-baseurl='https://github.com/rust-analyzer/rust-analyzer/releases/latest/download'
-binary='rust-analyzer-x86_64-unknown-linux-gnu.gz'
-tempdir=$(mktemp -d)
-
-mkdir -p ~/bin
-cd ~/bin || exit
-curl -L "$baseurl/$binary" -o "$tempdir/$binary"
-gzip -d "$tempdir/$binary" -c > ~/bin/rust-analyzer
-chmod +x ~/bin/rust-analyzer
-

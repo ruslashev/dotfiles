53d0c5a0fbb74720a47a105681c14da4b761d6ea
Ruslan Akbashev <ruslashev@gmail.com>
2023-02-14 06:41:02 +0400

    scripts: remove pips.sh and build-alacritty.sh
    * pips.sh was required by deoplete
    
    * alacritty is now installed via apt

--- scripts/new-system.sh
+++ scripts/new-system.sh
@@ -4,36 +4,30 @@ set -e
 
 cd ~/dotfiles/scripts/
 
-echo '=== 1/11: packages.sh'
+echo '=== 1/9: packages.sh'
 ./packages.sh
 
-echo '=== 2/11: pips.sh'
-./pips.sh
-
-echo '=== 3/11: zshrc.sh'
+echo '=== 2/9: zshrc.sh'
 ./zshrc.sh
 
-echo '=== 4/11: set-links.sh'
+echo '=== 3/9: set-links.sh'
 ./set-links.sh
 
-echo '=== 5/11: git.sh'
+echo '=== 4/9: git.sh'
 ./git.sh
 
-echo '=== 6/11: alacritty.sh'
+echo '=== 5/9: alacritty.sh'
 ./alacritty.sh
 
-echo '=== 7/11: rust.sh'
+echo '=== 6/9: rust.sh'
 ./rust.sh
 
-echo '=== 8/11: rust-update.sh'
+echo '=== 7/9: rust-update.sh'
 ./rust-update.sh
 
-echo '=== 9/11: monaco.sh'
+echo '=== 8/9: monaco.sh'
 ./monaco.sh
 
-echo '=== 10/11: build-alacritty.sh'
-./build-alacritty.sh
-
-echo '=== 11/11: vim.sh'
+echo '=== 9/9: vim.sh'
 ./vim.sh
 

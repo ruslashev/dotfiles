f0eb04270726139acbfda384da78ea18cc2f08f8
Ruslan Akbashev <ruslashev@gmail.com>
2023-02-14 06:27:58 +0400

    scripts/packages: add some new packages, add firefox-esr removal

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -4,5 +4,8 @@ sudo apt install -y gcc git curl python3-pip vim-gtk3 exuberant-ctags cmake \
     libclang-dev pkg-config libfreetype6-dev libfontconfig1-dev \
     libxcb-xfixes0-dev python3 tmux speedcrunch xclip tree net-tools gdb \
     valgrind clang fd-find jq gpg bless meld ripgrep xorriso mtools grub-pc-bin \
-    gedit telegram-desktop gdmap sl htop strace ltrace gpg
+    gedit telegram-desktop gdmap sl htop strace ltrace gpg bat patchutils \
+    firefox kcachegrind gitk mold nasm shellcheck xxd alacritty
+
+sudo apt remove -y firefox-esr
 

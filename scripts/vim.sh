fd5cd55aae654208ced7a7f1724fb4403d27829f
Ruslan Akbashev <ruslashev@gmail.com>
2023-02-14 06:32:21 +0400

    vim: add paramount_low_contrast and install coc-settings

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -8,6 +8,8 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s $(pwd)/../vim/colors/solarized.vim ~/.vim/colors/solarized.vim
+ln -s $(pwd)/../vim/colors/paramount_low_contrast.vim ~/.vim/colors/solarized.vim
+ln -s $(pwd)/../vim/coc-settings.json ~/.vim/coc-settings.json
 
 vim -c PlugInstall ~/.vimrc
 

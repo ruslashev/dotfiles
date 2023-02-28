c0a7b270bc759cc61bd1104ebf63e99b73c32ee5
Ruslan Akbashev <ruslashev@gmail.com>
2023-02-28 09:39:14 +0400

    scripts/vim: fix faulty symlinks, don't install old solarized

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -7,9 +7,8 @@ mkdir -p ~/.vim/undo
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln -s $(pwd)/../vim/colors/solarized.vim ~/.vim/colors/solarized.vim
-ln -s $(pwd)/../vim/colors/paramount_low_contrast.vim ~/.vim/colors/solarized.vim
-ln -s $(pwd)/../vim/coc-settings.json ~/.vim/coc-settings.json
+ln -s ~/dotfiles/vim/colors/paramount_low_contrast.vim ~/.vim/colors/paramount_low_contrast.vim
+ln -s ~/dotfiles/vim/coc-settings.json ~/.vim/coc-settings.json
 
 vim -c PlugInstall ~/.vimrc
 
